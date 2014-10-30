98a4d6ac926b43496cca7e07bf1de5db3b577e48
Alexander Zastupailo <zastupaylo@gmail.com>
2014-10-30 19:03:34 +0100

    geolocation: implemented logic for finding the nearest cinema and store it in localStorage

--- assignment-2/first-layout/test/javascript/scripts.js
+++ assignment-2/first-layout/test/javascript/scripts.js
@@ -1,7 +1,28 @@
 /**
- * @author Frederik Zwilling
- */
-
+* Set cinema coordinates to storage
+**/
+window.onload = function(e) {
+	var cinemas = {
+		'collection': [
+			{
+				'cinema_name' : 'IMAX',
+				'lat': 50.777477,
+				'lon': 6.078240
+			},
+			{
+				'cinema_name': 'Cinema City',
+				'lat': 50.759564,
+				'lon': 6.072994
+			},
+			{
+				'cinema_name': 'National Cinema',
+				'lat': 50.779663,
+				'lon': 6.102961
+			}
+		]
+	};
+	localStorage.setItem('cinemas', JSON.stringify(cinemas));
+}
 
 function initReservatioCanvas() {
 	//create canvas element and context to draw in
@@ -108,4 +129,71 @@ function getSeatFromClick(e) {
 function Seat(column, row) {
 	this.row = row;
 	this.column = column;
+}
+
+// Geolocation
+/**
+* Select nearest cinema according to user coords
+**/
+function chooseNearestCinema() {
+	if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(findNearestCinema);
+    } else {
+        // red popovers
+    }
+}
+
+/**
+* Find nearest cinema according to user coordinates and save to storage
+**/
+function findNearestCinema(position) {
+	var storage = localStorage.getItem('cinemas');
+	var cinemas = JSON.parse(storage);
+	var smallestDistance = null;
+	var nearestCinema = null;
+
+	for (var i = 0; i < cinemas.collection.length; i++) {
+		var distance = countDistance(
+				position.coords.latitude, position.coords.longitude, 
+				cinemas.collection[i].lat, cinemas.collection[i].lon
+			);
+		if (smallestDistance == null) {
+			smallestDistance = distance;
+			nearestCinema = cinemas.collection[i].cinema_name;
+		}
+		else {
+			if (distance < smallestDistance) {
+				smallestDistance = distance;
+				nearestCinema = cinemas.collection[i].cinema_name;
+			}
+		}
+	}
+	localStorage.setItem('chosen_cinema', nearestCinema);
+	expandCinameOptions(nearestCinema);
+}
+
+/**
+* Count distance between two points
+**/
+function countDistance(lat1, lon1, lat2, lon2) {
+	var R = 6371;
+  	var a = 0.5 - Math.cos((lat2 - lat1) * Math.PI / 180)/2 + 
+    	Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
+    	(1 - Math.cos((lon2 - lon1) * Math.PI / 180))/2;
+
+  	return R * 2 * Math.asin(Math.sqrt(a));
+}
+
+/**
+* Select cinema with button
+**/
+function chooseCinemaOnButtonSelect() {
+//set cinema to localstorage
+}
+
+/**
+* Show all options of selected cinema (movies slider, reservation)
+**/
+function expandCinemaOptions() {
+
 }
