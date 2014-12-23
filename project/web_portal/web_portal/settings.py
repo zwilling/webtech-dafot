022a8e212804050e156a1f69fba078ca7c8bd461
Tatiana Vert <werttanya@gmail.com>
2014-12-23 23:13:21 +0100

    User authentication with django-registration

--- project/web_portal/web_portal/settings.py
+++ project/web_portal/web_portal/settings.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from os import path
 import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_PATH = path.realpath(path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -30,6 +31,31 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+
+DEFAULT_EMAIL = 'dafot.webtech@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dafot.webtech'
+EMAIL_HOST_PASSWORD = 'rwthaachenuniversity'
+EMAIL_USE_TLS = True
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.debug',
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
 
 # Application definition
 
@@ -40,6 +66,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.humanize',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -62,8 +91,12 @@ WSGI_APPLICATION = 'web_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webportaldb',
+        'USER': 'postgres',
+        'PASSWORD': 'masterkey',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -88,4 +121,13 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
+
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window;
+
+TEMPLATE_DIRS = (
+    PROJECT_PATH + '/templates/',
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
