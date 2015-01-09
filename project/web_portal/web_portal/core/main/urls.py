31d23da236f11d96b746cef8b12c47d2c52555d0
Zastupailo Alexander <zastupailo@gmail.com>
2015-01-09 01:53:11 +0100

    Updated course_page templatem added comming_soon template

--- project/web_portal/web_portal/core/main/urls.py
+++ project/web_portal/web_portal/core/main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from web_portal.core.main.views import index
+from web_portal.core.main.views import coming, index
 
 urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     url(r'^index$', index, name='index'),
+    url(r'^coming$', coming, name='coming'),
 )
--- project/web_portal/web_portal/core/main/views.py
+++ project/web_portal/web_portal/core/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "main/index.html")
+
+
+def coming(request):
+    return render(request, "main/coming.html")
