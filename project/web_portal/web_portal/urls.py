5a6fa25ad30f9ff9408334636a9fb9c1ad814f9e
Zastupailo Alexander <zastupailo@gmail.com>
2015-01-14 22:06:43 +0100

    Added processing of media files

--- project/web_portal/web_portal/urls.py
+++ project/web_portal/web_portal/urls.py
@@ -12,3 +12,9 @@ urlpatterns = patterns('',
     (r"^", include("web_portal.core.users.urls")),
     (r"^", include("web_portal.core.courses.urls")),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT})
+    )
